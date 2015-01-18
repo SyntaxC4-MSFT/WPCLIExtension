386bb4c117c87ad1db9ef7ae52735a91f3c2f909
Cory Fowler <cory@syntaxc4.net>
2015-01-18 02:47:19 -0800

    removing extra whitespace

--- Content/install.cmd
+++ Content/install.cmd
@@ -1,17 +1,9 @@
 # Install WP-CLI
 
-
-
 cd Commands
 
-
-
 IF EXISTS wp-cli.phar (
-
     rm -f wp-cli.phar
-
 )
-
-
 
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
